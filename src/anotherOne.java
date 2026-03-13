e79deedf122008df448adfa3ad2c245182b8f214
Alekokakhidze17@gmail.com <alekokakhidze17@gmail.com>
2026-03-13 16:06:28 +0400

    axalia

---
+++ src/anotherOne.java
@@ -0,0 +1,9 @@
+public class anotherOne {
+    public static void main(String[] args) {
+        String sakheli = "nika";
+        char grade = 'A';
+
+        System.out.println(sakheli + grade);
+
+    }
+}
